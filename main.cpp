aa86be01f082eb6b15aad00a50e7314630bfe370
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2023-05-10 14:21:20 +0300

    Tekijätietoja muokattu

--- main.cpp
+++ main.cpp
@@ -21,15 +21,9 @@
  *
  * Ohjelman kirjoittajat
  *
- * Nimi: Elli Lehtimäki
- * Opiskelijanumero: 151309919
- * Käyttäjätunnus: nxelle
- * E-Mail: elli.i.lehtimaki@tuni.fi
+ * Nimi: EILeh
  *
- * Nimi: Henri Dyster
- * Opiskelijanumero: H211843
- * Käyttäjätunnus: fghedy
- * E-mail: henri.dyster@tuni.fi
+ * Nimi: Henri D....r
  *
  */
 
